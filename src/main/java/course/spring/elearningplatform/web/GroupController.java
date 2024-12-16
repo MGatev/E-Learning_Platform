7d9a5fc5f75bf5e08aca2ee433bf4686b9aa21f1
Venelina Andreeva <venelina.andreeva@sap.com>
2024-12-16 23:57:54 +0200

    Articles: Add more things in group controller
    # Conflicts:
    #	src/main/java/course/spring/elearningplatform/service/GroupService.java
    #	src/main/java/course/spring/elearningplatform/service/impl/ArticleServiceImpl.java

--- src/main/java/course/spring/elearningplatform/web/GroupController.java
+++ src/main/java/course/spring/elearningplatform/web/GroupController.java
@@ -1,22 +1,32 @@
 package course.spring.elearningplatform.web;
 
+import course.spring.elearningplatform.service.ArticleService;
+import course.spring.elearningplatform.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/groups")
 public class GroupController {
+    private GroupService groupService;
+    private ArticleService articleService;
+
+    @Autowired
+    public GroupController(GroupService groupService, ArticleService articleService) {
+        this.groupService = groupService;
+        this.articleService = articleService;
+    }
+
     @GetMapping("/{id}/articles")
     private String getAllArticles() {
         return "articles";
     }
 
     @DeleteMapping("/{id}/articles/{articleId}")
-    private String deleteArticle() {
-        return "groups";
+    private String deleteArticle(@PathVariable Long id, @PathVariable Long articleId) {
+        articleService.deleteArticleById(articleId);
+        return "redirect:/groups/" + id + "/articles";
     }
 
     @PostMapping("/{id}/articles/create")
