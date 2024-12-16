253f21447e9b92b5359cb25d6877226c7a7ca049
Martin Gatev <martin.gatev@sap.com>
2024-12-16 23:56:21 +0200

    Add impl

--- src/main/java/course/spring/elearningplatform/service/impl/ArticleServiceImpl.java
+++ src/main/java/course/spring/elearningplatform/service/impl/ArticleServiceImpl.java
@@ -3,6 +3,7 @@ package course.spring.elearningplatform.service.impl;
 import course.spring.elearningplatform.dto.ArticleDto;
 import course.spring.elearningplatform.dto.mapper.EntityMapper;
 import course.spring.elearningplatform.entity.Article;
+import course.spring.elearningplatform.exception.EntityNotFoundException;
 import course.spring.elearningplatform.repository.ArticleRepository;
 import course.spring.elearningplatform.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,12 @@ import java.util.List;
 @Service
 public class ArticleServiceImpl implements ArticleService {
     private final ArticleRepository articleRepository;
+    //private final GroupService groupService;
 
     @Autowired
     public ArticleServiceImpl(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
+        //this.groupService = groupService;
     }
 
     @Override
@@ -26,13 +29,19 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
-    public List<Article> getAllArticles() {
-        return articleRepository.findAll();
+    public List<Article> getAllArticlesForAGroup(Long groupId) {
+        // Group group = groupService.getGroupById(groupId);
+
+        return articleRepository.findAllByGroupId(groupId);
     }
 
     @Override
     public Article deleteArticleById(Long id) {
-        articleRepository.delete(article);
+        Article article = articleRepository.findById(id).orElse(null);
+        if (article == null) {
+            throw new EntityNotFoundException(String.format("Article with id=%s not found", id));
+        }
+        articleRepository.deleteById(id);
         return article;
     }
 }
