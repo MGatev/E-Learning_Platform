a47c2273964e109b9978f2db4875e486736fecdb
Venelina Andreeva <venelina.andreeva@sap.com>
2024-12-17 23:35:11 +0200

    Articles: Added Articles html and css

--- src/main/java/course/spring/elearningplatform/service/impl/GroupServiceImpl.java
+++ src/main/java/course/spring/elearningplatform/service/impl/GroupServiceImpl.java
@@ -1,4 +1,26 @@
 package course.spring.elearningplatform.service.impl;
 
-public class GroupServiceImpl {
+import course.spring.elearningplatform.dto.GroupDto;
+import course.spring.elearningplatform.entity.Group;
+import course.spring.elearningplatform.service.GroupService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GroupServiceImpl implements GroupService {
+    @Override
+    public List<Group> getAllGroups() {
+        return List.of();
+    }
+
+    @Override
+    public Group getGroupById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Group createGroup(GroupDto groupDto) {
+        return null;
+    }
 }
