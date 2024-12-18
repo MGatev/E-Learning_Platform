9931970f6f9b59447b85c1afa96667e228b70030
Venelina Andreeva <venelina.andreeva@sap.com>
2024-12-18 18:28:53 +0200

    Articles&Groups: Added creating of groups and impl of the controller

--- src/main/java/course/spring/elearningplatform/entity/Group.java
+++ src/main/java/course/spring/elearningplatform/entity/Group.java
@@ -1,13 +1,22 @@
 package course.spring.elearningplatform.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.Base64;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @AllArgsConstructor
@@ -22,13 +31,19 @@ public class Group {
   @EqualsAndHashCode.Include
   private String name;
 
-  private String imageUrl;
+  @Lob
+  private byte[] image;
+
+  private String description;
 
   @ManyToMany(fetch = FetchType.EAGER)
   @CollectionTable(name = "group_members")
-  private Set<User> members;
+  private List<User> members;
 
   @OneToMany(mappedBy = "group")
   private List<Article> articles;
-}
 
+  public String parseImage() {
+    return image != null ? Base64.getEncoder().encodeToString(image) : null;
+  }
+}
--- src/main/java/course/spring/elearningplatform/web/GroupController.java
+++ src/main/java/course/spring/elearningplatform/web/GroupController.java
@@ -1,22 +1,49 @@
 package course.spring.elearningplatform.web;
 
+import course.spring.elearningplatform.dto.GroupDto;
 import course.spring.elearningplatform.service.ArticleService;
 import course.spring.elearningplatform.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/groups")
 public class GroupController {
-    private GroupService groupService;
-    private ArticleService articleService;
+    private final GroupService groupService;
+    private final ArticleService articleService;
 
     @Autowired
     public GroupController(GroupService groupService, ArticleService articleService) {
         this.groupService = groupService;
         this.articleService = articleService;
     }
+
+    @GetMapping
+    public String getAllGroups(Model model) {
+        model.addAttribute("groups", groupService.getAllGroups());
+        return "groups";
+    }
+
+    @GetMapping("/{id}")
+    public String getGroupById(@PathVariable Long id, Model model) {
+        model.addAttribute("group", groupService.getGroupById(id));
+        return "group";
+    }
+
+    @GetMapping("/create")
+    public String showCreateGroupPage(Model model) {
+        model.addAttribute("group", new GroupDto());
+        return "create-group";
+    }
+
+    @PostMapping("/create")
+    public String createGroup(@ModelAttribute GroupDto groupDto) {
+        groupService.createGroup(groupDto);
+        return "redirect:/groups";
+    }
+
 
     @GetMapping("/{id}/articles")
     private String getAllArticles() {
