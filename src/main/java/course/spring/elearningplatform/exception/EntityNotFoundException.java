096ea8fb38c2fc55620604008947c7af9b116872
Manoela <manoelabarakova03@gmail.com>
2024-12-24 12:30:23 +0200

    Add exception constructor for message only

--- src/main/java/course/spring/elearningplatform/exception/EntityNotFoundException.java
+++ src/main/java/course/spring/elearningplatform/exception/EntityNotFoundException.java
@@ -4,10 +4,14 @@ import lombok.Getter;
 
 @Getter
 public class EntityNotFoundException extends RuntimeException {
-  private final String redirectUrl;
+  private String redirectUrl;
 
   public EntityNotFoundException(String message, String redirectUrl) {
     super(message);
     this.redirectUrl = redirectUrl;
+  }
+
+  public EntityNotFoundException(String message) {
+    super(message);
   }
 }
