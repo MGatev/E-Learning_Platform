4e6eaf12b57e5deae3c32191ee6177ca80907def
Manoela <manoelabarakova03@gmail.com>
2024-12-24 17:22:29 +0200

    Remove unused home endpoint

--- src/main/java/course/spring/elearningplatform/web/AuthController.java
+++ src/main/java/course/spring/elearningplatform/web/AuthController.java
@@ -1,10 +1,8 @@
 package course.spring.elearningplatform.web;
 
 import course.spring.elearningplatform.dto.UserDto;
-import course.spring.elearningplatform.entity.Course;
 import course.spring.elearningplatform.exception.DuplicateEmailException;
 import course.spring.elearningplatform.exception.DuplicateUsernameException;
-import course.spring.elearningplatform.service.CourseService;
 import course.spring.elearningplatform.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +14,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-import java.util.Map;
-
 @Controller
 public class AuthController {
     private final UserService userService;
-    private final CourseService courseService;
 
     @Autowired
-    public AuthController(UserService userService, CourseService courseService) {
+    public AuthController(UserService userService) {
         this.userService = userService;
-        this.courseService = courseService;
     }
 
     @GetMapping("/register")
@@ -35,13 +28,6 @@ public class AuthController {
         model.addAttribute("user", new UserDto());
         return "register";
     }
-
-    /*@GetMapping("/home")
-    public String home(Model model) {
-        Map<String, List<Course>> coursesByCategory = courseService.getCoursesGroupedByCategory();
-        model.addAttribute("coursesByCategory", coursesByCategory);
-        return "home";
-    }*/
 
     @PostMapping("/register")
     public String registerUser(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
