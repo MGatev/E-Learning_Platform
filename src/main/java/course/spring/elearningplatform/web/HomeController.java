bff9f997f7021a919daf21a91e3be87c99bfa5a8
Manoela <manoelabarakova03@gmail.com>
2024-12-24 17:30:29 +0200

    Update the home endpoint to include the courses grouped by category

--- src/main/java/course/spring/elearningplatform/web/HomeController.java
+++ src/main/java/course/spring/elearningplatform/web/HomeController.java
@@ -1,8 +1,10 @@
 package course.spring.elearningplatform.web;
 
+import course.spring.elearningplatform.entity.Course;
 import course.spring.elearningplatform.entity.Event;
+import course.spring.elearningplatform.service.CourseService;
 import course.spring.elearningplatform.service.EventService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor
 @Controller
 public class HomeController {
 
     private final EventService eventService;
+    private final CourseService courseService;
+
+    @Autowired
+    public HomeController(EventService eventService, CourseService courseService) {
+        this.eventService = eventService;
+        this.courseService = courseService;
+    }
 
     @GetMapping("/home")
     public String homePage(Model model) {
@@ -30,6 +39,10 @@ public class HomeController {
 
         model.addAttribute("events", allEvents);
         model.addAttribute("upcomingEvents", upcomingEvents);
+
+        Map<String, List<Course>> coursesByCategory = courseService.getCoursesGroupedByCategory();
+        model.addAttribute("coursesByCategory", coursesByCategory);
+
         return "home";
     }
 }
