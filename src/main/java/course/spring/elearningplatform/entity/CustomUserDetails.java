6def5b92af8c27a6385de5f1b5a62ff57f66c665
Martin Gatev <martin.gatev@sap.com>
2025-01-21 13:17:36 +0200

    Fix

--- src/main/java/course/spring/elearningplatform/entity/CustomUserDetails.java
+++ src/main/java/course/spring/elearningplatform/entity/CustomUserDetails.java
@@ -1,13 +1,11 @@
 package course.spring.elearningplatform.entity;
 
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@Getter
 public record CustomUserDetails(User user) implements UserDetails {
 
     @Override
