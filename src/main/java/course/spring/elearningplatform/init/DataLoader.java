33f395bfe2aadeb5ad1abeb37cf76fcd8c98b72e
Manoela <manoelabarakova03@gmail.com>
2025-02-10 18:28:41 +0200

    Add a deleted user when a course's owner is deleted.

--- src/main/java/course/spring/elearningplatform/init/DataLoader.java
+++ src/main/java/course/spring/elearningplatform/init/DataLoader.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Component
@@ -25,12 +26,28 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        createAdmin();
+        createDeletedUser();
+    }
+
+    private void createAdmin() {
         List<User> admins = userRepository.findByRole(Role.ADMIN.getDescription());
 
         if (admins == null || admins.isEmpty()) {
             User admin = new User("admin", passwordEncoder.encode("admin"),
                     "Admin", "Admin", "admin@mail.com", Set.of(Role.ADMIN.getDescription()));
             userRepository.save(admin);
+        }
+    }
+
+    // Creates a user that is used for reassigning courses to him when a user that has created a course is deleted
+    private void createDeletedUser() {
+        Optional<User> deletedUser = userRepository.findByUsername("deletedUser");
+
+        if (deletedUser.isEmpty()) {
+            User user = new User("deletedUser", passwordEncoder.encode("deletedUser"),
+                    "Deleted", "User", "deleted@mail.com", Set.of(Role.INSTRUCTOR.getDescription()));
+            userRepository.save(user);
         }
     }
 }
