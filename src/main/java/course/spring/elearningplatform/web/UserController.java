25046203c0a10a1dc2956e8e7e6eb92fc39600b4
Manoela <manoelabarakova03@gmail.com>
2025-02-10 18:24:55 +0200

    Merge file

--- src/main/java/course/spring/elearningplatform/web/UserController.java
+++ src/main/java/course/spring/elearningplatform/web/UserController.java
@@ -1,14 +1,20 @@
 package course.spring.elearningplatform.web;
 
+import course.spring.elearningplatform.entity.Role;
 import course.spring.elearningplatform.dto.ImageDto;
 import course.spring.elearningplatform.entity.User;
 import course.spring.elearningplatform.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -50,6 +57,37 @@ public class UserController {
                 .toList();
 
         return ResponseEntity.ok(allUsernames);
+    }
+
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("deletedUserFullName", userService.getUserById(id).getFullName());
+        userService.deleteUser(userService.getUserById(id));
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/users/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String showUser(@PathVariable Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditUser(@PathVariable Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        model.addAttribute("allRoles", Arrays.stream(Role.values()).filter(role -> role != Role.UNREGISTERED).toList());
+        return "edit-user";
+    }
+
+    @PostMapping("/users/edit/{id}")
+    public String editUser(@PathVariable Long id, @Valid @ModelAttribute("user") User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "edit-user";
+        }
+
+        userService.updateUser(user);
+        return "redirect:/admin/users";
     }
 
     @GetMapping("/users/{id}/certificates")
