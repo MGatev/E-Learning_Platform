5c3e78b3593d457c37283d71422273509d009d22
Manoela <manoelabarakova03@gmail.com>
2025-02-10 18:32:17 +0200

    Add method for finding all courses created by a given user

--- src/main/java/course/spring/elearningplatform/repository/CourseRepository.java
+++ src/main/java/course/spring/elearningplatform/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package course.spring.elearningplatform.repository;
 
 import course.spring.elearningplatform.entity.Course;
+import course.spring.elearningplatform.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,5 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     boolean existsByName(String name);
     @Query("SELECT c FROM Course c JOIN c.categories cat WHERE cat = :category")
     List<Course> findAllByCategory(String category);
+    List<Course> findAllByCreatedBy(User user);
 }
